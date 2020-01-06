932075bb170665d61088d8198761935a92585104
Suika <1223212366@qq.com>
2020-01-06 20:55:30 +0800

    第一次提交

---
+++ src/main/java/life/suika/echo/echo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package life.suika.echo.echo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/** vbfd
+ * Created by Suika on 2020/1/6.
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
