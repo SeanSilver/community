6a793c3c3dd2daf1f9843a09383737084010ea13
SeanSilver <1223212366@qq.com>
2020-01-08 23:08:29 +0800

    清理了垃圾并添加了session

--- src/main/java/life/suika/echo/echo/controller/AuthorizeController.java
+++ src/main/java/life/suika/echo/echo/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Suika on 2020/1/8.
  */
@@ -30,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest httpServletRequest){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
@@ -39,8 +42,14 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_id(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            //登录成功，写cookie和session
+            httpServletRequest.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            //登陆失败，重新登录
+            return "redirect:/";
+        }
     }
 }
 
