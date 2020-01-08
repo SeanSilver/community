bf8d406f698230e46347d4447453b7c2d6a3d21a
SeanSilver <1223212366@qq.com>
2020-01-08 21:51:20 +0800

    清理垃圾兼封装了xml

--- src/main/java/life/suika/echo/echo/controller/AuthorizeController.java
+++ src/main/java/life/suika/echo/echo/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.suika.echo.echo.dto.AccessTokenDTO;
 import life.suika.echo.echo.dto.GithubUser;
 import life.suika.echo.echo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +18,25 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("85c9ebe7067f8a105f38");
+        accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_secret("3ede1ef9af9624a638336f38453d7b152ac54214");
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_id("http://localhost:8888/callback");
+        accessTokenDTO.setRedirect_id(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
--- src/main/java/life/suika/echo/echo/provider/GithubProvider.java
+++ src/main/java/life/suika/echo/echo/provider/GithubProvider.java
@@ -32,7 +32,7 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token=6808847af0fb822607a276bf848fae0adf00ae58")
+                .url("https://api.github.com/user?access_token="+accessToken)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
