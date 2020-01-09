4cedebc6e42f2ee9e494d1152ad0c71b36512f6b
SeanSilver <1223212366@qq.com>
2020-01-09 18:59:47 +0800

    进行了持久化操作

--- src/main/java/life/suika/echo/echo/controller/AuthorizeController.java
+++ src/main/java/life/suika/echo/echo/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -41,7 +42,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest httpServletRequest){
+                           HttpServletResponse httpServletResponse){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
@@ -53,13 +54,14 @@ public class AuthorizeController {
         if (githubUser != null) {
             //登录成功，写cookie和session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setAccouontId(String.valueOf(githubUser.getId()));
             user.setName(githubUser.getName());
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            httpServletRequest.getSession().setAttribute("user", githubUser);
+            httpServletResponse.addCookie(new Cookie("token",token));
             return "redirect:/";
         } else {
             //登陆失败，重新登录
--- src/main/java/life/suika/echo/echo/controller/IndexController.java
+++ src/main/java/life/suika/echo/echo/controller/IndexController.java
@@ -1,7 +1,13 @@
 package life.suika.echo.echo.controller;
 
+import life.suika.echo.echo.mapper.UserMapper;
+import life.suika.echo.echo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /** vbfd
  * Created by Suika on 2020/1/6.
@@ -9,8 +15,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest httpServletRequest) {
+        Cookie[] cookies = httpServletRequest.getCookies();
+        try {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        httpServletRequest.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            //return "index";
+        }
         return "index";
     }
 }
